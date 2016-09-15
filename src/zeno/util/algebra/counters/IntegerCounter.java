7ad0f6d0a19d3ded9709463bdfd6c0bb63716025
Zeno <zenothedefiant@gmail.com>
2016-09-15 23:17:30 +0200

    Moved counters to Utilities - Algebra.

---
+++ src/zeno/util/algebra/counters/IntegerCounter.java
@@ -0,0 +1,39 @@
+package zeno.util.algebra.counters;
+
+import java.util.Iterator;
+
+import zeno.util.algebra.Integers;
+
+/**
+ * The {@code IntegerCounter} class creates a counter for integer values.
+ *
+ * @since May 5, 2016
+ * @author Zeno
+ * 
+ * @see Iterator
+ * @see Integer
+ */
+public class IntegerCounter implements Iterator<Integer>
+{
+	private int next;
+	
+	/**
+	 * Creates a new {@code IntegerCounter}.
+	 */
+	public IntegerCounter()
+	{
+		next = 0;
+	}
+	
+	@Override
+	public boolean hasNext()
+	{
+		return next != Integers.MAX_VALUE;
+	}
+
+	@Override
+	public Integer next()
+	{
+		return next++;
+	}
+}
