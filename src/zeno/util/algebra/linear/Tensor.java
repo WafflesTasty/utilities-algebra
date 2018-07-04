a40c31470519bc61b1521172948522dd2a66f4ea
Zeno <Zeno@Jade>
2018-07-04 15:30:46 +0200

    Refactoring project.

--- src/zeno/util/algebra/linear/Tensor.java
+++ src/zeno/util/algebra/linear/Tensor.java
@@ -4,65 +4,76 @@ import zeno.util.algebra.Measurable;
 import zeno.util.tools.Array;
 
 /**
- * The {@code Tensor} interface defines a vector element defined
- * as a multi-dimensional floating point array.
+ * The {@code Tensor} class defines an abstract superclass representing multilinear value components.
+ * All matrix and vector objects are designed as an instance of this class.
  *
  * @author Zeno
- * @since Apr 29, 2018
+ * @since Jul 4, 2018
  * @version 1.0
  * 
  * @param <T>  the type of the tensor
  * @see Measurable
  */
-public interface Tensor<T extends Tensor<T>> extends Measurable<T>
+public abstract class Tensor<T extends Tensor<T>> implements Measurable<T>
 {
 	/**
-	 * Returns the {@code Tensor} values.
+	 * Returns the order of the {@code Tensor}.
+	 * The order represents how many indices are needed
+	 * to define a value in the tensor.
 	 * 
-	 * @return  the tensor values
+	 * @return  the tensor order
 	 */
-	public abstract float[] Values();
+	public int Order()
+	{
+		return Dimensions().length;
+	}
 	
 	/**
-	 * returns the {@code Tensor} dimensions.
+	 * Returns the size of the {@code Tensor}.
+	 * The size represents how many values are stored
+	 * inside the tensor.
+	 * 
+	 * @return  the tensor size
+	 */
+	public int Size()
+	{
+		return Array.product.of(Dimensions());
+	}
+		
+
+	
+	/**
+	 * Returns the dimensions of the {@code Tensor}.
 	 * 
 	 * @return  the tensor dimensions
 	 */
 	public abstract int[] Dimensions();
 	
+	/**
+	 * Returns a single value in the {@code Tensor}.
+	 * 
+	 * @param coords  a tensor coördinate
+	 * @return  a tensor value
+	 */
+	public abstract float get(int... coords);
 	
 	/**
-	 * Checks if a {@code Tensor} has equal dimensions.
+	 * Returns all values in the {@code Tensor}.
 	 * 
-	 * @param t  a tensor to compare with
-	 * @return  {@code true} if the dimensions are equal
+	 * @return  the tensor values
 	 */
-	public default boolean hasEqualDimension(Tensor<T> t)
-	{
-		return Array.equals.of(Dimensions(), t.Dimensions());
-	}
+	public abstract float[] Values();
+		
 	
-	/**
-	 * Returns the {@code Tensor} order.
-	 * The order reflects the number of dimensions
-	 * the tensor is defined by.
-	 * 
-	 * @return  the tensor's order
-	 */
-	public default int Order()
+	@Override
+	public String toString()
 	{
-		return Dimensions().length;
+		return Array.parse.of(Values());
 	}
 
-	/**
-	 * Returns the {@code Tensor} size.
-	 * The size reflects the number of values
-	 * the tensor is defined by.
-	 * 
-	 * @return  the tensor's size
-	 */
-	public default int Size()
+	@Override
+	public int hashCode()
 	{
-		return Array.product.of(Dimensions());
+		return Array.hash.of(Values());
 	}
 }
