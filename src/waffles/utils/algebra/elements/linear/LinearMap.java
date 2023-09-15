73c1aae3b25d1dca1b12286039806080d805fd5f
WafflesTasty <zenothedefiant@gmail.com>
2023-09-15 09:34:05 +0200

    Removed generics from LinearMap, update 1.1.

--- src/waffles/utils/algebra/elements/linear/LinearMap.java
+++ src/waffles/utils/algebra/elements/linear/LinearMap.java
@@ -14,17 +14,14 @@ import waffles.utils.algebra.elements.linear.matrix.Matrix;
  * @version 1.1
  * 
  * 
- * @param <X>  a map source type
- * @param <Y>  a map target type
- * 
- * 
  * @see Function
  * @see Affine
  */
-public interface LinearMap<X extends Affine, Y extends Affine> extends Function<X, Y>
+//public interface LinearMap<X extends Affine, Y extends Affine> extends Function<X, Y>
+public interface LinearMap extends Function<Affine, Affine>
 {
 	/**
-	 * Returns the inverse matrix of the {@code LinearMap2}.
+	 * Returns the inverse matrix of the {@code LinearMap}.
 	 * 
 	 * @param dim  a matrix dimension
 	 * @return  a square matrix inverse
@@ -35,7 +32,7 @@ public interface LinearMap<X extends Affine, Y extends Affine> extends Function<
 	public abstract Matrix Inverse(int dim);
 	
 	/**
-	 * Returns the matrix of the {@code LinearMap2}.
+	 * Returns the matrix of the {@code LinearMap}.
 	 * 
 	 * @param dim  a matrix dimension
 	 * @return  a square matrix
@@ -47,20 +44,22 @@ public interface LinearMap<X extends Affine, Y extends Affine> extends Function<
 
 
 	@Override
-	public default X unmap(Y val)
+	public default Affine unmap(Affine val)
 	{
 		Matrix span = val.Span();
 		Factory fct = val.Factory();
+		
 		Matrix map = Inverse(span.Rows());
-		return (X) fct.create(map.times(span));
+		return fct.create(map.times(span));
 	}
 	
 	@Override
-	public default Y map(X val)
+	public default Affine map(Affine val)
 	{
 		Matrix span = val.Span();
 		Factory fct = val.Factory();
+		
 		Matrix map = Matrix(span.Rows());
-		return (Y) fct.create(map.times(span));
+		return fct.create(map.times(span));
 	}
 }
