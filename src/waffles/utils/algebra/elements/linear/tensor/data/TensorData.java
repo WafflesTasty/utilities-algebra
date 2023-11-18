8cade1a48aa2c1d05867468fd12ce47c1a8a42d9
WafflesTasty <zenothedefiant@gmail.com>
2023-11-18 09:46:17 +0100

    Minor javadoc update 1.1.

--- src/waffles/utils/algebra/elements/linear/tensor/data/TensorData.java
+++ src/waffles/utils/algebra/elements/linear/tensor/data/TensorData.java
@@ -15,6 +15,15 @@ import waffles.utils.sets.arrays.FloatArray;
 public interface TensorData extends FloatArray
 {
 	/**
+	 * Returns the non-zero count of the {@code TensorData}.
+	 * This can be an upper bound estimate, it is simply used
+	 * to determine the efficiency of tensor algorithms.
+	 * 
+	 * @return  a non-zero index count
+	 */
+	public abstract int NZCount();
+	
+	/**
 	 * Iterates over non-zero keys in the {@code TensorData}.
 	 * 
 	 * @return  an index key iterable
@@ -23,16 +32,7 @@ public interface TensorData extends FloatArray
 	 * @see Iterable
 	 */
 	public abstract Iterable<int[]> NZKeys();
-	
-	/**
-	 * Returns the non-zero index count of the {@code TensorData}.
-	 * This can be an upper bound estimate, it is simply used
-	 * to determine the efficiency of tensor algorithms.
-	 * 
-	 * @return  a non-zero index count
-	 */
-	public abstract int NZCount();
-	
+		
 	
 	@Override
 	public default TensorData copy()
