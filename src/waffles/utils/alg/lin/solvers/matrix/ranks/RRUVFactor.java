242174ecf79a06e9c97209e006bd6b5df9ca4d27
WafflesTasty <zenothedefiant@gmail.com>
2025-12-08 09:08:27 +0100

    Fixed Matrix complement, update 1.1.

--- src/waffles/utils/alg/lin/solvers/matrix/ranks/RRUVFactor.java
+++ src/waffles/utils/alg/lin/solvers/matrix/ranks/RRUVFactor.java
@@ -3,6 +3,7 @@ package waffles.utils.alg.lin.solvers.matrix.ranks;
 import waffles.utils.alg.lin.measure.matrix.Matrices;
 import waffles.utils.alg.lin.measure.matrix.Matrix;
 import waffles.utils.alg.lin.solvers.matrix.factor.UVFactor;
+import waffles.utils.tools.primitives.Integers;
 
 /**
  * An {@code RRUVFactor} algorithm computes rank through an {@code UVFactor}.
@@ -27,12 +28,13 @@ public interface RRUVFactor extends RankReveal, UVFactor
 	 */
 	public default Matrix Complement()
 	{
-		int r1 = Hints().Matrix().Rows();
 		int c1 = Hints().Matrix().Columns();
+		int r1 = Hints().Matrix().Rows();
+		int m = Integers.min(r1, c1);
 
 		// Create the column complement matrix...
-		Matrix b = Matrices.create(r1, c1 - rank());
-		for(int c = rank(); c < c1; c++)
+		Matrix b = Matrices.create(r1, m - rank());
+		for(int c = rank(); c < m; c++)
 		{
 			// ...as the last r - rank columns of U.
 			for(int r = 0; r < r1; r++)
@@ -58,7 +60,7 @@ public interface RRUVFactor extends RankReveal, UVFactor
 		int r1 = Hints().Matrix().Rows();
 		int c1 = Hints().Matrix().Columns();
 
-		// Create the column complement matrix...
+		// Create the column basis matrix...
 		Matrix b = Matrices.create(r1, rank());
 		for(int c = 0; c < rank(); c++)
 		{
